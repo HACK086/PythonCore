be7073543538af808884e90cc49edf120a3ef95e
Zhalil <ZHALIL.P.1303@GMAIL.COM>
2022-06-14 20:14:59 +0300

    Read Quality Control

--- Read Quality Control/main.py
+++ Read Quality Control/main.py
@@ -20,13 +20,31 @@ def analysis(your_list, your_dict):
             your_dict[i] += 1
         else:
             your_dict[i] = 1
-
-
+my_file.close()
+ccc = 0
 lst = l
 dct = {}
-
 analysis(lst, dct)
+count1 = 0
+count2= 0
+aver = []
+k = 0
+for j in data:
+    count1 = 0
+    for i in j:
+        if i == 'G' or i == 'C':
+            count1+=1
+    aver.append((count1/len(j)) * 100)
+for i in aver:
+    k = k + i
+k = k/len(aver)
+k = round(k,2)
+
 print(f'Reads in the file = {a}:')
-for item in sorted(dct):
-    print("      with length %d = %d" % (item, dct[item]))
-print('Reads sequence average length =', max(dct))
+count = [k*v for k,v in dct.items()]
+for j in range(0,len(count)):
+    ccc = ccc + count[j]
+ccc = ccc/a
+print('Reads sequence average length =', round(ccc))
+print()
+print(f'GC content average = {k}%')
